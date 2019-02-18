f971884ea2cba4af01dfdd52f4d8fa8f72fa690e
bazal4o <todor.paskalev@gmail.com>
2019-02-18 23:48:35 +0200

    Add project after project clear

---
+++ SugarLevelTracker/SugarLevelTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using SugarLevelTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SugarLevelTracker.Data
+{
+	public class ApplicationDbContext: DbContext
+	{
+		public ApplicationDbContext() :
+		base("OktaConnectionString")
+		{
+		}
+
+		public static ApplicationDbContext Create()
+		{
+			return new ApplicationDbContext();
+		}
+
+		public DbSet<SugarLevel> SugarLevels { get; set; }
+	}
+}
